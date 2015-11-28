fc742eb092ab343dcf2e27b935be04ed590c7da7
Shivaprasath <shivaprasathblog@users.noreply.github.com>
2015-11-28 15:04:32 +0530

    Update MainActivity.java

--- src/com/example/radiogroup/MainActivity.java
+++ src/com/example/radiogroup/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity
 {
 
+	//STEP 1: Define the Radiogroup and RadioButton
 	RadioGroup rg;
 	RadioButton r1,r2;
 	@Override
@@ -18,11 +19,12 @@ public class MainActivity extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		//STEP 2 : Refer the RadioGroup and RadioButton from Layoutfile
 		rg=(RadioGroup)findViewById(R.id.radioGroup1);
 		r1=(RadioButton)findViewById(R.id.radio0);
 		r2=(RadioButton)findViewById(R.id.radio1);
 		
-		
+		//STEP 3 : Creating the Listener for RadioGroup
 		rg.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 			
 			@Override
@@ -30,11 +32,12 @@ public class MainActivity extends Activity
 			{
 				// TODO Auto-generated method stub
 				
+				//STEP 4 : Action to be performed when Radiobutton 1(r1) is selected 
 				if(r1.isChecked()==true)
 				{
 					Toast.makeText(MainActivity.this, "Male", Toast.LENGTH_LONG).show();
 				}
-				
+				//STEP 5 : Action to be performed when Radiobutton 2(r2) is selected
 				if(r2.isChecked()==false)
 				{
 					Toast.makeText(MainActivity.this, "Female", Toast.LENGTH_LONG).show();
